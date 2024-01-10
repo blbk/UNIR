6749c836685c314721b2c1c313181c151bac7763
blbk <javiermoy@gmail.com>
2024-01-10 23:36:56 +0100

    Home, favicon y componentes carrusel y head

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -8,32 +8,120 @@ import SupportIcon from './icons/IconSupport.vue'
 </script>
 
 <template>
-    <div class="container text-center">
-      <i class="bi bi-android2"></i>
+    <div id="principal" class="container-xl">
+      <div class="row align-items-center">
+        <div class="col-6">
+          <img src="../assets/img1.png" alt="Nombre de la web: reservueling.com" width="396px" height="75px">
+        </div>
+        <div class="col-6 text-end">
+          <img src="../assets/normal_u38.png" alt="Logotipo de reservueling"  height="75px">
+        </div>
+      </div>
       <div class="row">
-        <div class="col-sm-4 col-lg-6 columna" >columna sm 4</div>
-        <div class="col-sm-8 col-lg-6 columna">columna sm 8</div>
+        <div class="col-12">
+            <div id="carouselAutoplaying" class="carousel slide" data-bs-ride="carousel">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="../assets/normal_u1.jpg" class="foto-carrusel d-block w-100 img-fluid" alt="fotografía panorámica de París por la noche">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../assets/normal_u7.jpg" class="foto-carrusel d-block w-100 img-fluid" alt="fotografía panorámica de Londres por la noche y el río Támesis">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="../assets/panoramica-coliseo-arco-constantino-roma-italia.jpg" class="foto-carrusel d-block w-100 img-fluid" alt="Fotografía panorámica del Coliseo romano y el Arco de Trajano, Roma, Italia">
+                    </div>
+                </div>
+                
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselAutoplaying" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselAutoplaying" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
+         </div>
       </div>
     </div>
 </template>
 
 <style>
   body {
-    background: silver;
+    background-color: #253347;
+    color: white;
+    font-family: 'Montserrat', Arial, Helvetica, sans-serif;
   }
-  .row {
-        border: solid 3px green;
-        padding: 5px;
-        margin: 10px;
+   .row { 
+        padding: 0px;
+        margin: 10px 0;
       }
-  .columna {
-    background: rgb(25, 0, 191);
-    color: aliceblue;
-    font-size: 25px;
-    border: 5px solid #ebb000;
+  .carousel-item {
+    max-height: 40vh;
   }
-  i {
-    font-size: 200px;
-    color:green;
+  .foto-carrusel {
+    width: 100%;
+  }
+  .formulario-viaje-inicio {
+    background-color: #F2F2F2;
+    padding: 1em 0;
+    font-size: 14px;
+    margin: 2em 0;
+  }
+  .formulario-viaje-inicio h1 {
+    font-size: 24px;
+    font-weight: bold;
+  }
+  .formulario-viaje-inicio label {
+    font-size: 18px;
+    font-weight: bold;
+  }
+  .formulario-origen-destino {
+    border: solid 2px #1E98D7;
+  }
+  .formulario-origen-destino select {
+    width: 90%;
+  }
+  .btn-light {
+    border: solid 1px #1E98D7;
+  }
+  .btn-info:hover {
+    color: white;
+    background-color: #c00;
+  }
+  .acceso {
+    padding: 2em;
+    text-align: end;
+  }
+  .acceso label {
+    max-width: 8.5rem;
+  }
+  .acceso button {
+    margin: .2em;
+    width: 12.3rem;
+  }
+  .mejor-vuelo {
+    padding: 2em 1em;
+    text-align: right;
+  }
+  .mejor-vuelo button {
+    margin: .2em;
+    width: 12.3rem;
+  }
+
+  .seccion-clara {
+    background-color: white;
+    color: #253347;
+  }
+  /* Pie de página */
+  footer {
+    margin-top: 1em;
+  }
+  footer ul {
+    list-style: none;
+  }
+  footer li {
+    display: inline;
+    margin-left: 3em;
   }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,9 +3,76 @@ import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
+  <TheWelcome />
   <main>
-    <h1>Prueba de texto</h1>
-    <p> banana</p>
-    <TheWelcome />
+    <div class="container-lg">
+      <div class="row seccion-clara">
+        <div class="col-12 col-md-6 col-lg-8">
+          <div class="container formulario-viaje-inicio">
+            <h1>
+              ¡Busca tu destino deseado<br>y nosotros encontraremos lo mejor para ti!
+            </h1>
+            <div class="row">
+              <div class="col-12 col-lg-8">
+                <form action="" class="formulario-origen-destino">
+                  <div class="row justify-content-between" >
+                    <div class="col-12 col-md-7">
+                      <label for="origen">Origen</label>
+                      <select id="origen" class="form-select" aria-label="origen select">
+                        <option selected>Madrid</option>
+                        <option value="1">Londres</option>
+                        <option value="2">París</option>
+                        <option value="3">Roma</option>
+                      </select>
+                      <label for="destino">Destino</label>
+                      <select id="destino" class="form-select" aria-label="destino select">
+                        <option selected>Madrid</option>
+                        <option value="1">Londres</option>
+                        <option value="2">París</option>
+                        <option value="3">Roma</option>
+                      </select>
+                    </div>
+                    <div class="col-12 col-md-5">
+                      <label for="fecha-ida">Fecha de ida</label>
+                      <input type="date" id="fecha-ida">
+                      <label for="fecha-vuelta">Fecha de vuelta</label>
+                      <input type="date" id="fecha-vuelta">
+                    </div>
+                  </div>
+                </form>
+              </div>
+              <div class="col-12 col-lg-4">
+                <section class="mejor-vuelo">
+                  <button type="button" class="btn btn-info">
+                    Dime el mejor vuelo
+                  </button>
+                  <img src="../assets/normal_u38.png" alt="Logotipo de reservueling" height="30px">
+                </section>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-12 col-md-6 col-lg-4">
+          <section class="acceso">
+            <label>¿Ya eres cliente?</label> <button type="button" class="btn btn-info">Acceder a mis viajes</button><br>
+            <label>No soy cliente </label> <button type="button" class="btn btn-light">Quiero darme de alta</button>
+          </section>
+        </div>
+      </div>
+      <footer>
+        <div class="row">
+          <div class="col-2">
+            Inicio
+          </div>
+          <div class="col-10 text-end">
+            <ul>
+              <li>Condiciones</li>
+              <li>Quiénes somos</li>
+              <li>Política de protección de datos</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </div>
   </main>
 </template>
